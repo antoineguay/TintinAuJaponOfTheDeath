1146b1277d2ffc87824d45e4bb063badf7e213e7
Antoine <antoine.guellier@insa-rennes.fr>
2011-11-15 19:58:49 +0100

    first commmiiiit

---
+++ ProjetCPPUML/SpaceBattle/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceBattle
+{
+    class Class1
+    {
+        private void lilili(object sender, RoutedEventArgs e)
+        {
+            // Création du TextBlock
+            TextBlock tblock = new TextBlock();
+            tblock.Text = "Hello le SDZ";
+
+            // Ajout du contrôle à la fentre
+            this.Content = tblock;
+        }
+    }
+}
